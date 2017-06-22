51521f55f30d8fffeaa3ad9e76718df005ae1bcc
Ryan Mahoney <ryan@virtuecenter.com>
2017-06-21 22:55:52 -0400

    wip2

--- models/task.js
+++ models/task.js
@@ -5,45 +5,58 @@ const execSync = require('child_process').execSync;
 const spawnSync = require('child_process').spawnSync;
 
 module.exports = (aws, commit, repoPath, repoModel) => {
+
+  const run = (task) => {
+    console.log(task);
+    // start timer
+    const startTime = new Date().getTime() / 1000 | 0;
+
+    // run a task synchronously
+    const taskPath = repoPath + '/lsci/' + task.name;
+    const outputPath = taskPath + '/output';
+    const runPath = taskPath + '/run.sh';
+    const dirCommand = `mkdir -p ${outputPath}`
+    const touchCommand = `touch ${runPath}`
+    const chmodCommand = `chmod +x ${runPath}`
+    execSync(dirCommand);
+    execSync(touchCommand);
+    execSync(chmodCommand);
+    const result = spawnSync('./run.sh', {cwd: taskPath, shell: '/bin/bash', encoding: 'utf-8'});
+    const totalSeconds = (new Date().getTime() / 1000 | 0) - startTime;
+    const state = (result.status == 1) ? 'failure' : 'success';
+    const stdout = convert.toHtml(result.stdout);
+    console.log(stdout);
+    const stderr = convert.toHtml(result.stderr);
+    const status = {provider: commit.provider, tasks: [{
+      name: task.name,
+      state: state,
+      target_url: "",
+      description: task.messages[state],
+      context: 'lsci'}]};
+
+    var asyncTasks = [];
+    asyncTasks.push(repoModel.uploadStdout(task.name, stdout));
+    asyncTasks.push(repoModel.uploadStderr(task.name, stderr));
+
+    // call home with result
+    asyncTasks.push(request.post(commit.status_callback, status));
+
+    // TODO: save any other generated files to S3
+
+    // run subtasks
+    const subtasks = task.tasks || [];
+    subtasks.forEach((subtask) => {
+      console.log(subtask);
+      asyncTasks.push(run(subtask));
+    });
+
+    // return promise when all subtasks are done
+    return Promise.all(asyncTasks);
+  };
+
   return {
     run: (task) => {
-      // start timer
-      const startTime = new Date().getTime() / 1000 | 0;
-
-      // run a task synchronously
-      const taskPath = repoPath + '/lsci/' + task.name;
-      const outputPath = taskPath + '/output';
-      const runPath = taskPath + '/run.sh';
-      const dirCommand = `mkdir -p ${outputPath}`
-      const touchCommand = `touch ${runPath}`
-      const chmodCommand = `chmod +x ${runPath}`
-      execSync(dirCommand);
-      execSync(touchCommand);
-      execSync(chmodCommand);
-      const result = spawnSync('./run.sh', {cwd: taskPath, shell: '/bin/bash', encoding: 'utf-8'});
-      const totalSeconds = (new Date().getTime() / 1000 | 0) - startTime;
-      const state = (result.status == 1) ? 'failure' : 'success';
-      const stdout = convert.toHtml(result.stdout);
-      const stderr = convert.toHtml(result.stderr);
-      const status = {provider: commit.provider, tasks: [{
-        name: task.name,
-        state: state,
-        target_url: "",
-        description: task.messages[state],
-        context: 'lsci'}]};
-
-      var asyncTasks = [];
-      asyncTasks.push(repoModel.uploadStdout(task.name, stdout));
-      asyncTasks.push(repoModel.uploadStderr(task.name, stderr));
-
-      // call home with result
-      asyncTasks.push(request.post(commit.status_callback, status));
-
-      // TODO: save any other generated files to S3
-      // TODO: run subtasks
-
-      // return promise when all subtasks are done
-      return Promise.all(asyncTasks);
+      run(task);
     }
   };
 };
